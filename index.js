693891975b090ab0c942b959bad95171d9b393d2
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-04 15:02:37 -0500

    adding generateHTML.js to src and adding team profile const to index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Packages needed for this program
+// Node packages needed for this program
 const fs = require('fs');
 const inquirer = require('inquirer');
-const jest = require('jest');
+const generateHTML = require('./src/generateHTML');
+//const jest = require('jest');
+
+// Team profiles
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
